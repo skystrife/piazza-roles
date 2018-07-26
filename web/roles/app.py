c84cd43c491e59526c96dbbdf2ea5d2f37f27993
Chase Geigle <geigle1@illinois.edu>
2018-07-25 21:04:14 -0500

    Add initial class listing page and required models.

--- web/roles/app.py
+++ web/roles/app.py
@@ -1,26 +1,33 @@
+from .models import *
 from .celery import make_celery
 from flask import (Flask, flash, g, redirect, render_template, request,
                    session, url_for)
+from flask_breadcrumbs import Breadcrumbs, register_breadcrumb
 from flask_dotenv import DotEnv
 from flask_sqlalchemy import SQLAlchemy
 import functools
 import piazza_api
 import requests
+from operator import attrgetter
 import os
 
 app = Flask(__name__)
 env = DotEnv(app)
+db.init_app(app)
+Breadcrumbs(app)
 celery = make_celery(app)
 
 
 @app.before_request
-def load_piazza_api():
-    email = session.get('email')
+def load_user():
+    uid = session.get('uid')
     piazza_jar = session.get('piazza_jar')
 
-    if not email or not piazza_jar:
+    if not uid or not piazza_jar:
         g.piazza = None
+        g.user = None
     else:
+        g.user = User.query.get(uid)
         piazza_rpc = piazza_api.rpc.PiazzaRPC()
         piazza_rpc.cookies = requests.utils.cookiejar_from_dict(piazza_jar)
         g.piazza = piazza_api.Piazza(piazza_rpc)
@@ -29,7 +36,7 @@ def load_piazza_api():
 def login_required(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
-        if g.piazza is None:
+        if g.user is None:
             flash('You are not logged in.', 'danger')
             return redirect(url_for('index'))
 
@@ -39,6 +46,7 @@ def login_required(view):
 
 
 @app.route('/')
+@register_breadcrumb(app, '.', 'Home')
 def index():
     if g.piazza is not None:
         return redirect(url_for('classes'))
@@ -47,27 +55,58 @@ def index():
 
 @app.route('/login', methods=['POST'])
 def login():
-    session.pop('email', None)
+    session.pop('uid', None)
     session.pop('piazza_jar', None)
+
+    email = request.form['email']
+    password = request.form['password']
+
     piazza_rpc = piazza_api.rpc.PiazzaRPC()
     error = None
     try:
-        piazza_rpc.user_login(request.form['email'], request.form['password'])
+        piazza_rpc.user_login(email, password)
         session['piazza_jar'] = requests.utils.dict_from_cookiejar(
             piazza_rpc.cookies)
-        session['email'] = request.form['email']
+        g.piazza = piazza_api.Piazza(piazza_rpc)
+
+        user = User.query.filter_by(email=email).first()
+        if user is None:
+            user = User(email)
+            db.session.add(user)
+            db.session.commit()
+
+        session['uid'] = user.id
+
+        classes = g.piazza.get_user_classes()
+        for cls in (cls for cls in classes if cls['is_ta']):
+            net = Network.query.filter_by(nid=cls['nid']).first()
+            if net is None:
+                net = Network(
+                    nid=cls['nid'],
+                    number=cls['num'],
+                    name=cls['name'],
+                    term=cls['term'])
+                net.users.append(user)
+                db.session.add(net)
+
+        db.session.commit()
+
         return redirect(url_for('classes'))
     except piazza_api.exceptions.AuthenticationError:
         flash('Email or password incorrect', 'danger')
+        session.pop('uid', None)
+        session.pop('piazza_jar', None)
         return redirect(url_for('index'))
     except:
         flash('Could not communicate with Piazza API', 'danger')
+        session.pop('uid', None)
+        session.pop('piazza_jar', None)
         return redirect(url_for('index'))
 
 
 @app.route('/logout')
 def logout():
-    session.pop('email', None)
+    session.pop('uid', None)
     session.pop('piazza_jar', None)
     flash('You were successfully logged out.', 'success')
     return redirect(url_for('index'))
@@ -75,5 +114,37 @@ def logout():
 
 @app.route('/classes')
 @login_required
+@register_breadcrumb(app, '.classes', 'Classes')
 def classes():
-    return "TODO"
+    def term_to_tuple(term):
+        parts = term.split()
+        order = ['Other', 'Spring', 'Summer', 'Fall']
+        return (parts[1], order.index(parts[0]))
+
+    networks = sorted(g.user.networks, key=attrgetter('number'))
+    networks = sorted(
+        networks, key=lambda cls: term_to_tuple(cls.term), reverse=True)
+    return render_template('classes.html', classes=networks)
+
+
+def class_name_dlc(*args, **kwargs):
+    nid = request.view_args['id']
+    net = Network.query.get(nid)
+    if net is None:
+        return []
+    return [{
+        'text': "{} ({})".format(net.number, net.term),
+        'url': url_for('class', id=nid)
+    }]
+
+
+@app.route('/class/<nid>')
+@login_required
+@register_breadcrumb(
+    app, '.class.nid', '', dynamic_list_constructor=class_name_dlc)
+def view_class(nid):
+    net = Network.query.get(nid)
+    if net is None:
+        flash('We could not find that class', 'danger')
+        return redirect(url_for('classes'))
+    return render_template('class.html', network=net)
---
+++ web/roles/models.py
@@ -0,0 +1,52 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+network_user = db.Table(
+    'network_user',
+    db.Column(
+        'network_id',
+        db.Integer,
+        db.ForeignKey('network.id'),
+        primary_key=True),
+    db.Column(
+        'user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True))
+
+
+class Network(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nid = db.Column(db.String(20), index=True, unique=True, nullable=False)
+    number = db.Column(db.String(120), nullable=False)
+    name = db.Column(db.String(120), nullable=False)
+    term = db.Column(db.String(120), nullable=False)
+    users = db.relationship(
+        'User',
+        secondary=network_user,
+        lazy='subquery',
+        backref=db.backref('users', lazy=True))
+
+    def __init__(self, nid, number, name, term):
+        self.nid = nid
+        self.number = number
+        self.name = name
+        self.term = term
+
+    def __repr(self):
+        return "<Network: {}:{} ({} {})>".format(self.id, self.nid,
+                                                 self.number, self.term)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    networks = db.relationship(
+        'Network',
+        secondary=network_user,
+        lazy='subquery',
+        backref=db.backref('networks', lazy=True))
+
+    def __init__(self, email):
+        self.email = email
+
+    def __repr__(self):
+        return "<User: {}>".format(self.email)
